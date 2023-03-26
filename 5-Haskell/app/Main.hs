77b3ba009f78d399e20a0655c6accbb9e101d382
Nevernown <nevernown@technotive.nl>
2023-03-26 22:27:58 +0200

    ...

--- 5-Haskell/app/Main.hs
+++ 5-Haskell/app/Main.hs
@@ -16,8 +16,10 @@ port = 9001
 -- You can roughly think of the `<-` directive as "await" and `IO ()` as "async void"
 -- You can think of `$` as 
 --   "first calculate everything right of $ then input the result into the function left of $"
+--   This is actually exactly the same thing as using parenthesis () on the entrire right-hand side.
 -- You can think of `.` as 
 --   "make a new function which first does the thing to the right of . and then the thing to the left of ."
+--   The official term for this directive is "Composition", mathematically speaking: (f . g) n == f(g(n))
 
 main :: IO ()
 main = do
