10be13712dc1fa5041e1d11a15a9f07227b07e07
Nevernown <nevernown@technotive.nl>
2023-03-27 14:58:09 +0200

    Finished Rust; proceeding to rewrite all projects for readability

--- 4-Rust/src/helper.rs
+++ 4-Rust/src/helper.rs
@@ -7,3 +7,7 @@
  */
 
 pub const IN_ADDR_ANY: [u8; 4] = [0, 0, 0, 0];
+pub const ANSI_COLOR_RED   : &str = "\x1b[31m";
+pub const ANSI_COLOR_GREEN : &str = "\x1b[32m";
+pub const ANSI_COLOR_BLUE  : &str = "\x1b[34m";
+pub const ANSI_COLOR_RESET : &str = "\x1b[0m";
--- 4-Rust/src/main.rs
+++ 4-Rust/src/main.rs
@@ -1,11 +1,13 @@
 // Based on the Rust documentation:
 // https://doc.rust-lang.org/std/net/struct.TcpListener.html
 
+use std::io::prelude::*;
+use std::str;
 use std::net::{TcpListener, TcpStream, SocketAddr};
 
 mod helper; // See helper.rs for more information
+use crate::helper::*;
 
-const MAX_CONNECTIONS_PENDING: i32 = 8;
 const PORT: u16 = 9001;
 
 fn main() {
@@ -17,21 +19,53 @@ fn main() {
             listening(&listener);
         }
     } else {
-        println!("Something went wrong");
+        println!("Binding to interface failed");
     }
 }
 
 fn listening(listener: &TcpListener) {
     let some_connection = listener.accept();
     if let Ok(connection) = some_connection {
-        let (stream, info) = connection;
+        let (mut stream, info) = connection;
+        print!("{}", ANSI_COLOR_GREEN);
         println!("Client {} connected", info.ip());
-        
+        print!("{}", ANSI_COLOR_BLUE);
+        communicate(&mut stream);
+        print!("{}", ANSI_COLOR_RED);
+        println!("Client {} connected", info.ip());
+        print!("{}", ANSI_COLOR_RESET);
     } else {
         println!("Client connection failed");
     }
 }
 
-fn receive_data_on(stream: &TcpStream) {
-
+fn communicate(stream: &mut TcpStream) {
+    let mut buffer = [0;256];
+    let result = stream.read(&mut buffer);
+    if let Ok(byte_count) = result {
+        if let Ok(request) = str::from_utf8(&buffer) {
+            println!("Received {} bytes\n{}", byte_count, request);
+            let some_contents = request[0..byte_count].split_once(':');
+            if let Some(contents) = some_contents {
+                let some_size = contents.1.trim().parse();
+                if let Ok(size) = some_size {
+                    let mut response = contents.0.chars().take(size).collect::<String>();
+                    response.push('\n');
+                    if let Ok(_) = stream.write(response.as_bytes()) {
+                        // No need to check further
+                    } else {
+                        println!("Could not respond")
+                    }
+                } else {
+                    println!("Could not parse size");
+                }
+            } else {
+                println!("Could not split request");
+            }
+        } else {
+            println!("Could not decode request");
+        }
+    } else {
+        println!("Error while receiving data");
+    }
 }
